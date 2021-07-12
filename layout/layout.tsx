d6a032572c57ec312c4b8a9adbdc38400a4a60ab
xirurgx3d <xirurgx3d@gmail.com>
2021-07-12 14:38:40 +0300

    add layout

---
+++ layout/layout.tsx
@@ -0,0 +1,12 @@
+import { LayoutProps } from './layout.props'
+import styles from './layout.module.css'
+import cn from 'classnames'
+
+export default ({children}:LayoutProps): JSX.Element => {
+
+  return (
+    <>
+      
+    </>  
+  )
+}
